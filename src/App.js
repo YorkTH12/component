42bcae2f13ad4657fba4bf1c19c7d347f3ee5c05
YorkTH12 <148939113+YorkTH12@users.noreply.github.com>
2026-01-08 10:25:31 +0700

    events

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
+import Button from './class-components';
 
 import { MsgBox2 } from './func-components';
 
 function App() {
   return (
-    <MsgBox2
-      text="การใช้ props ใน Functional Component"
-      border="solid 1px black"
-      bgColor="#ccc"
-      color="red"
-      fontSize="18pt"
-    />
+    // <MsgBox2
+    //   text="การใช้ props ใน Functional Component"
+    //   border="solid 1px black"
+    //   bgColor="#ccc"
+    //   color="red"
+    //   fontSize="18pt"
+    // />
+    <Button/>
   );
 }
 
---
+++ src/class-components.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+export default class Button extends Component{
+    // constructor(){
+    //     super()
+    //     this.onClickButtonOK = this.onClickButtonOK.bind(this)
+    // }
+    showAlert(msg){
+        alert(msg)
+    }
+
+    onClickButtonOK = () => {
+        this.showAlert('Hello')
+    }
+
+    render(){
+        return <button onClick={this.onClickButtonOK}>OK</button>
+    }
+}
